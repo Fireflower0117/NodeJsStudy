9d3ddf1e3ac87713749626f23d607f514a487aea
jn.kim <poptome@opennote.co.kr>
2025-12-26 00:12:23 +0900

    길번 출판사 Node.js 교과서 개정3판 조현영 243Page express, nodemon설치 까지 완료

---
+++ public/javascripts/chap06/6_1_express/1_1_0_express.js
@@ -0,0 +1,11 @@
+/* 이제 본격적으로 express를 시작해보기로한다.
+   먼저  프로젝트 폴더를 생성하고  package.json을 생성해야 한다.
+   그러나 현재 학습용 프로젝트가 이미 package.json이 있으므로 이과정은 skip한다.
+
+   cmd(터미널)로  express 패키지를 설치한다.
+   {projecthome}> npm i express
+   {projecthome}> npm i -D nodemon
+
+
+
+*/
